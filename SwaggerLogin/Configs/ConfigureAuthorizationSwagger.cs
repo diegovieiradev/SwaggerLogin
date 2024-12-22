674ab2c22675d9fa70886f6d8d2f8ad8797b38b0
Diego Vieira <sr.diegovieira@gmail.com>
2024-12-22 20:39:19 -0300

    Implementado login e senha para poder ter acesso ao SWAGGER.

---
+++ SwaggerLogin/Configs/ConfigureAuthorizationSwagger.cs
@@ -0,0 +1,53 @@
+﻿namespace SwaggerLogin.Configs
+{
+    public class ConfigureAuthorizationSwagger
+    {
+        private readonly RequestDelegate _requestDelegate;
+
+        public ConfigureAuthorizationSwagger(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Path.StartsWithSegments("/swagger-login"))
+            {
+                if (context.Request.Method == "POST")
+                {
+                    var form = await context.Request.ReadFormAsync();
+                    var username = form["username"];
+                    var password = form["password"];
+
+                    if (username == "admin" && password == "senha")
+                    {
+                        context.Session.SetString("Authenticated", "true");
+                        context.Response.Redirect("/swagger");
+                        return;
+                    }
+
+                    var errorHtmlPath = Path.Combine(Directory.GetCurrentDirectory(), "Templates", "SwaggerAutenticacaoErrorView.html");
+                    var errorHtmlContent = await File.ReadAllTextAsync(errorHtmlPath);
+                    context.Response.ContentType = "text/html";
+                    await context.Response.WriteAsync(errorHtmlContent);
+                    return;
+                }
+
+                var htmlPath = Path.Combine(Directory.GetCurrentDirectory(), "Templates", "SwaggerAutenticacaoView.html");
+                var htmlContent = await File.ReadAllTextAsync(htmlPath);
+
+                context.Response.ContentType = "text/html";
+                await context.Response.WriteAsync(htmlContent);
+                return;
+            }
+
+            if (context.Request.Path.StartsWithSegments("/swagger") && !context.Session.Keys.Contains("Authenticated"))
+            {
+                context.Response.Redirect("/swagger-login");
+                return;
+            }
+
+            await _requestDelegate(context);
+        }
+    }
+}
---
+++ SwaggerLogin/Program.cs
@@ -0,0 +1,38 @@
+using SwaggerLogin.Configs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+var app = builder.Build();
+
+app.UseSession();
+app.UseMiddleware<ConfigureAuthorizationSwagger>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
